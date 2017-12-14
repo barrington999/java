646e49c09014d7e8278270b43f44ee28f24c3795
Daniel Jones <barrington999@gmail.com>
2017-12-14 20:27:38 +0000

    First commit

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Hello World!");
+        //Calendar now = new GregorianCalendar(Calendar.TUESDAY, Calendar.MONTH, Calendar.DAY_OF_MONTH);
+        Calendar now = new GregorianCalendar();
+
+        Date nowTime = now.getTime();
+        System.out.println(nowTime.toString());
+
+
+        for (int i = 0; i < ; i++) {
+
+        }
+
+        for (:
+             ) {
+
+        }
+        
+    }
+}
